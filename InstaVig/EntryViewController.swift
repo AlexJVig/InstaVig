67c157135483e77f9ab1954eba27371c98947d8e
Alex <alzombii@gmail.com>
2019-03-09 16:33:44 +0200

    Finally made that unwind segue work!

---
+++ InstaVig/EntryViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EntryViewController.swift
+//  InstaVig
+//
+//  Created by Alex Vig on 09/03/2019.
+//  Copyright © 2019 Alex Vig. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func cancelToLoginViewController(segue: UIStoryboardSegue) {
+        print("unwound")
+    }
+    
+    @IBAction func confirmToLoginViewController(_ segue: UIStoryboardSegue) {
+        print("unwound")
+    }
+
+}
